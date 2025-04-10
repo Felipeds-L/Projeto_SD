490f575997472d38a54a2ac2e008a012c29736c1
Felipe Leite <felipesd.leite@gmail.com>
2025-04-10 11:53:11 -0300

    fix: solving problem from writting testbench and ULA timestamp

--- modulos/ULA/ULA_testbench.sv
+++ modulos/ULA/ULA_testbench.sv
@@ -5,7 +5,7 @@ module ULA_tb;
 
 reg [31:0] entrada_01;
 reg [31:0] entrada_02;
-reg [2:0] ULA_cpntrol;
+reg [2:0] ULA_control;
 
 
 wire [31:0] ULA_result;
@@ -14,7 +14,7 @@ wire Zero;
 ULA uut (
     .entrada_01(entrada_01),
     .entrada_02(entrada_02),
-    .ULA_cpntrol(ULA_cpntrol),
+    .ULA_control(ULA_control),
     .ULA_result(ULA_result),
     .Zero(Zero)
 );
@@ -23,38 +23,38 @@ initial begin
     
     entrada_01 = 0;
     entrada_02 = 0;
-    ULA_cpntrol = 0;
+    ULA_control = 0;
 
     #10;
     entrada_01 = 32'hA5A5A5A5;
     entrada_02 = 32'h5A5A5A5A;
-    ULA_cpntrol = 3'b000;
+    ULA_control = 3'b000;
     #10;
 
-    ULA_cpntrol = 3'b001;
+    ULA_control = 3'b001;
     #10;
 
     entrada_01 = 32'h0000000F;
     entrada_02 = 32'h00000001;
-    ULA_cpntrol = 3'b010;
+    ULA_control = 3'b010;
     #10;
 
     entrada_01 = 32'h0000000F;
     entrada_02 = 32'h0000000F;
-    ULA_cpntrol = 3'b110;
+    ULA_control = 3'b110;
     #10;
 
     entrada_01 = 32'h0000000A;
     entrada_02 = 32'h0000000F;
-    ULA_cpntrol = 3'b111;
+    ULA_control = 3'b111;
     #10;
 
     entrada_01 = 32'hA5A5A5A5;
     entrada_02 = 32'h5A5A5A5A;
-    ULA_cpntrol = 3'b100;
+    ULA_control = 3'b100;
     #10;
 
-    ULA_cpntrol = 3'b011;
+    ULA_control = 3'b011;
     #10;
 
     $finish;
